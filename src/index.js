cf8c8d5f923dd866d1f214e8ddec664edcaad6cf
olton <serhii@pimenov.com.ua>
2024-08-11 23:47:08 +0300

    fix hsl primitives

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const Primitives = {
 }
 
 const version = "1.0.4"
-const build_time = "11.08.2024, 23:46:31"
+const build_time = "11.08.2024, 23:47:03"
 
 const info = () => {
     console.info(`%c Farbe %c v${version} %c ${build_time} `, "color: #ffffff; font-weight: bold; background: #ff00ff", "color: white; background: darkgreen", "color: white; background: #0080fe;")
